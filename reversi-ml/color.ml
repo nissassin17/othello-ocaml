46f7d8b02663cc909176eea0b80aa5f1c475bb52
Nissassin17 <nissassin17@gmail.com>
2016-08-01 17:08:50 +0900

    init

---
+++ reversi-ml/color.ml
@@ -0,0 +1,17 @@
+type color = int 
+let none     = 0
+let white    = 1 
+let black    = 2
+
+let opposite_color c = (2-c) + 1
+
+let string_of_color c = 
+  if c = white then "White"
+  else              "Black"
+
+
+let print_color c = 
+  if      c = white then print_string "O" 
+  else if c = black then print_string "X"
+  else if c = none  then print_string " " 
+  else ()
---
+++ reversi-ml/play.ml
@@ -0,0 +1,496 @@
+(*configuration start*)
+let threshold = 100000000
+
+(*source: http://www.samsoft.org.uk/reversi/strategy.htm*)
+let val_corner = 99
+let val_c = -8
+let val_x = -24
+let val_a = 8
+let val_a1 = -4
+let val_a2 = 7
+let val_b = 6
+let val_b1 = -3
+let val_b2 = 4
+let val_start = 0
+(*Named Openings
+ * source: http://www.samsoft.org.uk/reversi/openings.htm#openings *)
+
+(*original list
+C4c3	Diagonal Opening
+C4c3D3c5B2	X-square Opening
+C4c3D3c5B3	Snake/Peasant
+C4c3D3c5B3f3	Lysons
+C4c3D3c5B3f4B5b4C6d6F5	Pyramid/Checkerboarding Peasant
+C4c3D3c5B4	Heath/Tobidashi
+C4c3D3c5B4d2C2f4D6c6F5e6F7	Mimura Variation II
+C4c3D3c5B4d2D6	Heath-Bat
+C4c3D3c5B4d2E2	Iwasaki Variation
+C4c3D3c5B4e3	Heath-Chimney
+C4c3D3c5B5	Raccoon Dog
+C4c3D3c5B6	Rocket
+C4c3D3c5B6c6B5	Hamilton
+C4c3D3c5B6e3	Lollipop
+C4c3D3c5D6	Cow
+C4c3D3c5D6e3	Chimney
+C4c3D3c5D6f4B4	Cow Bat/Bat/Cambridge
+C4c3D3c5D6f4B4b6B5c6B3	Bat (Piau Continuation 2)
+C4c3D3c5D6f4B4b6B5c6F5	Melnikov/Bat (Piau Continuation 1)
+C4c3D3c5D6f4B4c6B5b3B6e3C2a4A5a6D2	Bat (Kling Continuation)
+C4c3D3c5D6f4B4e3B3	Bat (Kling Alternative)
+C4c3D3c5D6f4F5	Rose-v-Toth
+C4c3D3c5D6f4F5d2	Tanida
+C4c3D3c5D6f4F5d2B5	Aircraft/Feldborg
+C4c3D3c5D6f4F5d2G4d7	Sailboat
+C4c3D3c5D6f4F5e6C6d7	Maruoka
+C4c3D3c5D6f4F5e6F6	Landau
+C4c3D3c5F6	Buffalo/Kenichi Variation
+C4c3D3c5F6e2C6	Maruoka Buffalo
+C4c3D3c5F6e3C6f5F4g5	Tanida Buffalo
+C4c3D3c5F6f5	Hokuriku Buffalo
+C4c3E6c5	Wing Variation
+C4c3F5c5	Semi-Wing Variation
+C4c5	Parallel Opening
+C4e3	Perpendicular Opening
+C4e3F4c5D6e6	Mimura
+C4e3F4c5D6f3C6	Shaman/Danish
+C4e3F4c5D6f3D3	Inoue
+C4e3F4c5D6f3D3c3	Iago
+C4e3F4c5D6f3E2	Bhagat
+C4e3F4c5D6f3E6c3D3e2	Rose
+C4e3F4c5D6f3E6c3D3e2B5	Flat
+C4e3F4c5D6f3E6c3D3e2B5f5	Rotating Flat
+C4e3F4c5D6f3E6c3D3e2B5f5B3	Murakami Variation
+C4e3F4c5D6f3E6c3D3e2B5f5B4f6C2e7D2c7	Rotating Flat (Kling Continuation)
+C4e3F4c5D6f3E6c3D3e2B6f5	Rose-Birth
+C4e3F4c5D6f3E6c3D3e2B6f5B4f6G5d7	Brightstein
+C4e3F4c5D6f3E6c3D3e2B6f5G5	Rose-Birdie/Rose-Tamenori
+C4e3F4c5D6f3E6c3D3e2B6f5G5f6	Rose-Tamenori-Kling
+C4e3F4c5D6f3E6c3D3e2D2	Greenberg/Dawg
+C4e3F4c5D6f3E6c6	Ralle
+C4e3F4c5E6	Horse
+C4e3F5b4	Ganglion/No-Cat
+C4e3F5b4F3	Swallow
+C4e3F5b4F3f4E2e6G5f6D6c6	No-Cat (Continuation)
+C4e3F5e6D3	Italian
+C4e3F5e6F4	Cat
+C4e3F5e6F4c5D6c6F7f3	Sakaguchi
+C4e3F5e6F4c5D6c6F7g5G6	Berner
+C4e3F6b4	Bent Ganglion
+C4e3F6e6F5	Tiger
+C4e3F6e6F5c5C3	Stephenson
+C4e3F6e6F5c5C3b4	No-Kung
+C4e3F6e6F5c5C3b4D6c6B5a6B6c7	No-Kung (Continuation)
+C4e3F6e6F5c5C3c6	Comp'Oth
+C4e3F6e6F5c5C3c6D3d2E2b3C1c2B4a3A5b5A6a4A2	F.A.T. Draw
+C4e3F6e6F5c5C3c6D6	Lighning Bolt
+C4e3F6e6F5c5C3g5	Kung
+C4e3F6e6F5c5D3	Leader's Tiger
+C4e3F6e6F5c5D6	Brightwell
+C4e3F6e6F5c5F4g5G4f3C6d3D6	Ishii
+C4e3F6e6F5c5F4g5G4f3C6d3D6b3C3b4E2b6	Mainline Tiger
+C4e3F6e6F5c5F4g6F7	Rose-Bill
+C4e3F6e6F5c5F4g6F7d3	Tamenori
+C4e3F6e6F5c5F4g6F7g5	Central Rose-Bill/Dead Draw
+C4e3F6e6F5g6	Aubrey/Tanaka
+C4e3F6e6F5g6E7c5	Aubrey (Feldborg Continuation)
+*)
+
+(*black start*)
+let openings = (
+    ((3, 4); (3, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 2));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 3); (6, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 3); (6, 4); (2, 5); (2, 4); (3, 6); (4, 6); (6, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 4));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 4); (4, 2); (3, 2); (6, 4); (4, 6); (3, 6); (6, 5); (5, 6); (6, 7));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 4); (4, 2); (4, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 4); (4, 2); (5, 2));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 4); (5, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 6); (3, 6); (2, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (2, 6); (5, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (5, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (2, 4));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (2, 4); (2, 6); (2, 5); (3, 6); (2, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (2, 4); (2, 6); (2, 5); (3, 6); (6, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (2, 4); (3, 6); (2, 5); (2, 3); (2, 6); (5, 3); (3, 2); (1, 4); (1, 5); (1, 6); (4, 2));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (2, 4); (5, 3); (2, 3));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5); (4, 2));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5); (4, 2); (2, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5); (4, 2); (7, 4); (4, 7));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5); (5, 6); (3, 6); (4, 7));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (4, 6); (6, 4); (6, 5); (5, 6); (6, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (6, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (6, 6); (5, 2); (3, 6));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (6, 6); (5, 3); (3, 6); (6, 5); (6, 4); (7, 5));
+    ((3, 4); (3, 3); (4, 3); (3, 5); (6, 6); (6, 5));
+    ((3, 4); (3, 3); (5, 6); (3, 5));
+    ((3, 4); (3, 3); (6, 5); (3, 5));
+    ((3, 4); (3, 5));
+    ((3, 4); (5, 3));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (5, 6));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (3, 6));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (4, 3));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (4, 3); (3, 3));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 2));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 5));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 5); (6, 5));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 5); (6, 5); (2, 3));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 5); (6, 5); (2, 4); (6, 6); (3, 2); (5, 7); (4, 2); (3, 7));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 6); (6, 5));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 6); (6, 5); (2, 4); (6, 6); (7, 5); (4, 7));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 6); (6, 5); (7, 5));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (2, 6); (6, 5); (7, 5); (6, 6));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 3); (4, 3); (5, 2); (4, 2));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (4, 6); (6, 3); (5, 6); (3, 6));
+    ((3, 4); (5, 3); (6, 4); (3, 5); (5, 6));
+    ((3, 4); (5, 3); (6, 5); (2, 4));
+    ((3, 4); (5, 3); (6, 5); (2, 4); (6, 3));
+    ((3, 4); (5, 3); (6, 5); (2, 4); (6, 3); (6, 4); (5, 2); (5, 6); (7, 5); (6, 6); (4, 6); (3, 6));
+    ((3, 4); (5, 3); (6, 5); (5, 6); (4, 3));
+    ((3, 4); (5, 3); (6, 5); (5, 6); (6, 4));
+    ((3, 4); (5, 3); (6, 5); (5, 6); (6, 4); (3, 5); (4, 6); (3, 6); (6, 7); (6, 3));
+    ((3, 4); (5, 3); (6, 5); (5, 6); (6, 4); (3, 5); (4, 6); (3, 6); (6, 7); (7, 5); (7, 6));
+    ((3, 4); (5, 3); (6, 6); (2, 4));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (2, 4));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (2, 4); (4, 6); (3, 6); (2, 5); (1, 6); (2, 6); (3, 7));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (3, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (3, 6); (4, 3); (4, 2); (5, 2); (2, 3); (3, 1); (3, 2); (2, 4); (1, 3); (1, 5); (2, 5); (1, 6); (1, 4); (1, 2));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (3, 6); (4, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (3, 3); (7, 5));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (4, 3));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (4, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (6, 4); (7, 5); (7, 4); (6, 3); (3, 6); (4, 3); (4, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (6, 4); (7, 5); (7, 4); (6, 3); (3, 6); (4, 3); (4, 6); (2, 3); (3, 3); (2, 4); (5, 2); (2, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (6, 4); (7, 6); (6, 7));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (6, 4); (7, 6); (6, 7); (4, 3));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (3, 5); (6, 4); (7, 6); (6, 7); (7, 5));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (7, 6));
+    ((3, 4); (5, 3); (6, 6); (5, 6); (6, 5); (7, 6); (5, 7); (3, 5))
+)
+(*configuration end*)
+
+open Array
+open Color
+open Command
+open Int64
+
+(*const*)
+type board_t = int64 * int64
+let pos_corner = (1, 1)
+let pos_c = (2, 1)
+let pos_x = (2, 2)
+let pos_a = (3, 1)
+let pos_a1 = (3, 2)
+let pos_a2 = (3, 3)
+let pos_b = (4, 1)
+let pos_b1 = (4, 2)
+let pos_b2 = (4, 3)
+let pos_start = (4, 4)
+
+let val_max = 99 * 64 + 1
+let val_min = -99 * 64 - 1
+
+let all_pos =
+    let mix xs ys =
+        List.concat (List.map (fun x -> List.map (fun y -> (x,y)) ys) xs)
+    in
+    let ls = [1;2;3;4;5;6;7;8] in
+    (mix ls ls)
+let board_empty = (zero, zero)
+module Board = struct
+    type t = board_t
+    let compare b1 b2 =
+        let (x1, y1), (x2, y2) = board_encode b1, board_encode b2 in
+        if x1 != x2 then
+            compare x1 x2
+        else
+            compare y1 y2
+end;;
+module Minimax = Map.Make Board;;
+module OpeningMap = Map.Make Board
+module PosMap = Map.Make (struct t = int * int let compare p1 p2 =
+    compare (pos_sym_encode p1) (pos_sym_encode p2)
+    end)
+let pos_val_map =
+    let pos_list = [pos_corner; pos_c; pos_x; pos_a; pos_a1; pos_a2; pos_b; pos_b1; pos_b2; pos_start] in
+    let val_list = [val_corner; val_c; val_x; val_a; val_a1; val_a2; val_b; val_b1; val_b2; val_start] in
+    List.fold_left (fun map (pos, value) -> PosMap.add pos value map ) PosMap.empty (List.map2 (fun pos value -> (pos, value)) pos_list val_list)
+
+(*always started by black*)
+(* map board to list of board*)
+let opening_map =
+    let opening_map_add board next_board map =
+        let current_list =
+            if OpeningMap.mem board map then
+                OpeningMap.find board map
+            else
+                []
+        in
+        OpeningMap.add board (next_board::current_list) map
+    in
+    List.fold_left (fun map moves_array ->
+        let (m, b, c)  = List.fold_left (fun (map, board) pos ->
+            if is_valid_move board pos then
+                let bb = minimax_move board pos in
+                opening_map_add board bb map, bb
+            else
+                map, board
+            ) (board_init ()) moves_array
+        in
+            m
+    ) OpeningMap.empty openings
+
+(**************BIT**************)
+(*set bit on*)
+let bit_set (num: int64) (pos: int) =
+    logor num (shift_left one pos)
+
+(*get bit*)
+let bit_get (num: int64) (pos: int) =
+    (logand (shift_right_logical num pos) one) = one
+
+(****************POS***************)
+(*pos encode and decode*)
+let pos_encode (x, y) =
+    (x - 1) * 8 + y - 1
+
+let pos_decode (pos: int) =
+    (pos / 8 + 1, pos mod 8 + 1)
+let pos_rotate_one (x, y) = (9 - y, x)
+let pos_flip (x, y) = (y, x)
+let pos_sym_encode (x, y) =
+    let mx, my = Pervasives.min x (9 - x), Pervasives.min y (9 - y) in
+        pos_encode (Pervasives.max mx my, Pervasives.min mx my)
+
+
+(*******************BOARD*******************)
+(*move by black*)
+let board_move_black ((bb, bw) : board_t) pos = (bit_set bb (pos_encode pos), bw)
+
+let board_move_white (bb, bw) pos = (bb, bit_set bw (pos_encode pos))
+
+let board_get_color ((bb, bw) : board_t) opos =
+    let ipos = pos_encode opos in
+    if bit_get bb ipos then
+        black
+    else
+        if bit_get bw ipos then
+            white
+        else
+            none
+
+let board_rotate_one board =
+    List.fold_left (fun bb pos -> board_move_black bb (pos_rotate_one pos) (board_get_color board pos)) board_empty all_pos
+
+(*tail recursive*)
+let board_rotated (board: board_t) cnt current=
+    Pervasives.fst (List.fold_left (fun (blist, bb) _ -> (bb::blist, board_rotate_one bb)) ([], board) [1;1;1;1])
+
+let board_swap (bb, bw) = (bw, bb)
+
+let board_encode (board: board_t) =
+    Pervasives.min ((board_rotated board) @ (board_rotated (board_flipped board)))
+
+let board_move_white
+(*init board*)
+let board_init () =
+    board_move_black
+        (board_move_black
+            (board_move_white
+                (board_move_white board_empty (4, 4))
+                (5, 5))
+            (4, 5))
+        (5, 4)
+
+let dirs = [ (-1,-1); (0,-1); (1,-1); (-1,0); (1,0); (-1,1); (0,1); (1,1) ]
+
+let flippable_indices_line board (di,dj) (i,j) =
+  let rec f (di,dj) (i,j) r =
+    if (board_get_color board (i, j)) = white then
+      g (di,dj) (i+di,j+dj) ( (i,j) :: r )
+    else
+      []
+  and    g (di,dj) (i,j) r =
+    if (board_get_color board (i, j)) = white then
+      g (di,dj) (i+di,j+dj) ( (i,j) :: r )
+    else if (board_get_color board (i, j)) = black then
+      r
+    else
+      [] in
+    f (di,dj) (i,j) []
+
+let flippable_indices board (i,j) =
+  let bs = List.map (fun (di,dj) -> flippable_indices_line board (di,dj) (i+di,j+dj)) dirs in
+    List.concat bs
+
+let is_effective board (i,j) =
+  match flippable_indices board (i,j) with
+      [] -> false
+    | _  -> true
+
+let board_refine board color =
+    if color = white then board_swap board else board
+
+let is_valid_move board (i,j) =
+  ((board_get_color board (i, j)) = none) && is_effective board (i,j)
+
+
+let board_do_move board com =
+  match com with
+      GiveUp  -> board
+    | Pass    -> board
+    | Mv (i,j) ->
+        let ms = flippable_indices board (i,j) in
+            List.fold_left (fun bb (ii, jj) -> board_move_black bb (ii, jj)) board ms
+
+let valid_moves board =
+  List.filter (is_valid_move board) all_pos
+
+let board_move board pos =
+    board_swap (board_do_move board (Mv pos))
+(*use bfs to search*)
+(*tail recursive*)
+let rec minimax_expand map queue board_list cnt =
+    if cnt >= threshold || Queue.is_empty queue then
+        board_list, map
+    else
+        let board = Queue.pop queue in
+        let mm, bbl, cc =
+            List.fold_left (fun mm bbl cc pos ->
+                let bb = minimax_move board pos in
+                    if OpeningMap.mem bb mm then
+                        mm, bbl, cc
+                    else
+                        let _ = Queue.push bb queue in
+                        let bval = board_eval bb in
+                        let mmm = OpeningMap.add bb (bval, bval) mm in
+                        let bbbl = bb::bbl in
+                        mmm, bbbl, cc + 1
+            ) (map, board_list, cnt) (valid_moves board)
+        in
+            minimax_expand mm queue bbl cc
+
+let board_eval_core board =
+    List.fold_left (fun v pos ->
+        if board_get_color board pos = black then
+            v + (pos_val pos)
+        else
+            v
+           ) 0 all_pos
+let board_eval board =
+    (board_eval_core board) - (board_eval_core (board_swap board))
+
+(*pop all board from board_list then evaluate using minimax*)
+let rec minimax map board_list
+    [] -> map
+    | board::rest ->
+        List.fold_left (fun map pos -> ) map (valid_moves board)
+
+(*return a map that maps all next moves to theirs min, max value*)
+let board_minimax board =
+    let init_queue = Queue.create () in
+    let _ = Queue.push board init_queue in
+    let init_map = Minimax.add board (val_max, val_min) Minimax.empty in
+    let board_list, map = minimax_expand init_map init_queue [] 1 in
+        List.fold_left (fun map bb ->
+            let bcount, wcount = count bb black, count bb white in
+            if bcount + wcount = 64 then (*end game*)
+                if bcount > wcount then Minimax.add bb (val_max, val_max) (*win*)
+                else
+                    if bcount = wcount then Minimax.add bb (0, 0) (*tie*)
+                    else Minimax.add bb (val_min, val_min) (*lose*)
+            else
+                let List.fold_left (fun mm pos ->
+                    let bbb = minimax_move bb pos in
+                    if Minimax.mem bbb then
+                        let new_min, new_max = Minimax.find bbb in
+                        let old_min, old_max = Minimax.find bb in
+                            if new_min < old_min || new_max > old_max then
+                                Minimax.add bb (Pervasives.min new_min old_min, Pervasives new_max old_max)
+                    else
+                        mm
+        ) map (valid_moves bb)
+    ) map board_list
+
+let play board' color =
+    let board = board_refine board' color in
+    let ms =
+        if OpeningMap.mem board openingMap then
+            let bb::_ = OpeningMap.find board openingMap in
+            List.fold_left (fun current pos -> if Pervasives.fst current != 0 then
+                current
+            else
+                if board_encode (minimax_move board pos) = board_encode bb then
+                    pos::current
+                else
+                    current
+            ) [] all_pos
+        else
+        let minimax = board_minimax board in
+            (List.fold_left (fun current pos ->
+                match current with
+                [] -> [pos]
+                | any::_ ->
+                    let (_, current_max) = Minimax.find (minimax_move board pos) minimax in
+                    let (_, iter_max) =  Minimax.find (minimax_move board pos) minimax in
+                    if current_max = iter_max then
+                        pos :: current
+                    else
+                        if current_max < iter_max then
+                            current
+                        else
+                            [pos]
+            ) [] (valid_moves board)
+    in
+    if ms = [] then
+      Pass
+    else
+        let k = Random.int (List.length ms) in
+        let (i,j) = List.nth ms k in
+            Mv (i,j)
+
+let count board color =
+  let s = ref 0 in
+    for i=1 to 8 do
+      for j=1 to 8 do
+        if (board_get_color board (i, j)) = color then s := !s + 1
+      done
+    done;
+    !s
+
+
+let print_board board =
+  print_endline " |A B C D E F G H ";
+  print_endline "-+----------------";
+  for j=1 to 8 do
+    print_int j; print_string "|";
+    for i=1 to 8 do
+      print_color (board_get_color board (i, j)); print_string " "
+    done;
+    print_endline ""
+  done;
+  print_endline "  (X: Black,  O: White)"
+
+
+let report_result board =
+  let _ = print_endline "========== Final Result ==========" in
+  let bc = count board black in
+  let wc = count board white in
+    if bc > wc then
+      print_endline "*Black wins!*"
+    else if bc < wc then
+      print_endline "*White wins!*"
+    else
+      print_endline "*Even*";
+    print_string "Black: "; print_endline (string_of_int bc);
+    print_string "White: "; print_endline (string_of_int wc);
+    print_board board
+
